52b5574db426587c56cd275938ce24a36ac656b8
shivam <spandya@redhat.com>
2022-02-02 01:19:02 +0530

    real time board update

--- app.py
+++ app.py
@@ -1,50 +1,64 @@
-import configparser
+import configparser, os
 from boxit.boxit import boxit
 
-config = configparser.RawConfigParser()   
-config.read('word.conf')
+def gameplay():
+    config = configparser.RawConfigParser()   
+    config.read('word.conf')
 
-wordle = config.get('secret', 'word').upper()
-word = list(wordle)
-count = 0
+    wordle = config.get('secret', 'word').upper()
+    word = list(wordle)
+    count = 0
 
-guess = input("Take your guess: \n ").upper()
+    grids = [["0", "0", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"]]
 
-grids = [["0", "0", "0", "0", "0"],
-         ["0", "0", "0", "0", "0"],
-         ["0", "0", "0", "0", "0"],
-         ["0", "0", "0", "0", "0"],
-         ["0", "0", "0", "0", "0"],
-         ["0", "0", "0", "0", "0"]]
+    clscr()
+    print_grid(grids)
 
-while count != 5:
-    if guess.lower() == 'q' or guess.lower() == 'quit':
-        print("You quit the game")
-        break
-    if len(guess) != 5:
-        print("Word length should exactly be 5 characters, plese try again")
-        guess = input("Take your guess: \n ").upper()
-    for i in range(len(guess)):
-        if guess[i] in word:
-            if guess[i] == word[i]:
-                grids[count][i] = boxit(guess[i], 'green')
+    guess = input("\nTake your guess: \n ").upper()
+    clscr()
+
+    while count != 5:
+        if guess.lower() == 'q' or guess.lower() == 'quit':
+            print("You quit the game")
+            break
+        if len(guess) != 5:
+            print("Word length should exactly be 5 characters, plese try again")
+            guess = input("Take your guess: \n ").upper()
+        for i in range(len(guess)):
+            if guess[i] in word:
+                if guess[i] == word[i]:
+                    grids[count][i] = boxit(guess[i], 'green')
+                else:
+                    grids[count][i] = boxit(guess[i], 'yellow')
             else:
-                grids[count][i] = boxit(guess[i], 'yellow')
-        else:
-            grids[count][i] = guess[i]
-    
+                grids[count][i] = guess[i]
+        
+        print_grid(grids)
+
+        if guess == ''.join(word):
+            print(boxit("YOU WON!", "green", pattern = 'solid', shift=2, spacing=2))
+            print(boxit(f"  Totatl tries: {count+1}", "orange"))
+            break
+        count += 1
+        if count == 6:
+            print(boxit("Sorry, you didn't make it! Better luck next time:)", 'red'))
+            break
+        print()
+        guess = input("Take your guess: \n ").upper()
+        clscr()
+
+def print_grid(grids):
     for i in grids:
         for j in i:
             print(f"  {j}", end="")
         print()
 
-    if guess == ''.join(word):
-        print(boxit("YOU WON!", "green", pattern = 'solid', shift=2, spacing=2))
-        print(boxit(f"  Totatl tries: {count+1}", "orange"))
-        break
-    count += 1
-    if count == 6:
-        print(boxit("Sorry, you didn't make it! Better luck next time:)", 'red'))
-        break
-    print()
-    guess = input("Take your guess: \n ").upper()
+def clscr():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+gameplay()
