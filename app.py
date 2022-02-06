6c39f14cc4a5473af6bd6235c6d6f9c1bcbf904e
Michal Fabik <mfabik@redhat.com>
2022-02-06 20:01:51 +0100

    Linting with pylint and flake8

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import os, random
+import os
+import random
+
 from boxit.boxit import boxit
+
 
 def gameplay():
 
@@ -35,39 +38,43 @@ def gameplay():
             print("Guess not allowed! Please try again")
             guess = input("Take your guess: \n ").upper()
             continue
-        for i in range(len(guess)):
-            if guess[i] in word:
-                if guess[i] == word[i]:
-                    grids[count][i] = boxit(guess[i], 'green')
-                    keyboard = keyboard.replace(guess[i], boxit(guess[i], 'green'))
+        for position, letter in enumerate(guess):
+            if letter in word:
+                if letter == word[position]:
+                    grids[count][position] = boxit(letter, 'green')
+                    keyboard = keyboard.replace(letter, boxit(letter, 'green'))
                     score += "g"
                 else:
-                    grids[count][i] = boxit(guess[i], 'yellow')
-                    if not boxit(guess[i], 'green') in keyboard:
-                        keyboard = keyboard.replace(guess[i], boxit(guess[i], 'yellow'))
+                    grids[count][position] = boxit(letter, 'yellow')
+                    if not boxit(letter, 'green') in keyboard:
+                        keyboard = keyboard.replace(letter,
+                                                    boxit(letter, 'yellow'))
                     score += "y"
             else:
-                grids[count][i] = guess[i]
-                keyboard = keyboard.replace(guess[i], '■')
+                grids[count][position] = letter
+                keyboard = keyboard.replace(letter, '■')
                 score += 'n'
         print_grid(grids)
         print()
         print(keyboard)
 
         if guess == ''.join(word):
-            print(boxit("YOU WON!", "green", pattern = 'solid', shift=2, spacing=2))
+            print(boxit("YOU WON!", "green", pattern='solid',
+                        shift=2, spacing=2))
             print(boxit(f"  Total tries: {count+1}", "orange"))
             print(display_score(score))
             break
         count += 1
         if count == 6:
-            print(boxit("Sorry, you didn't make it! Better luck next time:)", 'red'))
-            print("The word was: "+ boxit(wordle, 'green'))
+            print(boxit("Sorry, you didn't make it! Better luck next time:)",
+                        'red'))
+            print("The word was: " + boxit(wordle, 'green'))
             print("\nPress enter to exit!")
             input()
             break
         print()
         guess = input("Take your guess: \n ").upper()
+
 
 def print_grid(grids):
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -76,31 +83,33 @@ def print_grid(grids):
             print(f"  {j}", end="")
         print()
 
+
 def get_word():
     with open('wordlist.txt', 'r+') as words:
         wordlist = words.read().splitlines()
         return random.choice(wordlist).upper()
 
+
 def check_word(word):
-    guess_file = open('allowed.txt', 'r+')
-    guesslist = guess_file.read().splitlines()
-    guess_file.close()
-    words = open('wordlist.txt', 'r+')
-    word_list = words.read().splitlines()
-    words.close()
+    with open('allowed.txt', 'r+') as guess_file:
+        guesslist = guess_file.read().splitlines()
+    with open('wordlist.txt', 'r+') as words:
+        word_list = words.read().splitlines()
     if word in guesslist or word in word_list:
         return True
     return False
 
+
 def display_score(score):
-    colors = {'g':'🟩', 'y':'🟨', 'n':'⬜'}
+    colors = {'g': '🟩', 'y': '🟨', 'n': '⬜'}
     score += 'n'*(30-len(score))
     score = list(score)
-    for i in range(len(score)):
-        score[i] = colors[score[i]]       
+    for count, value in enumerate(score):
+        score[count] = colors[value]
     scorecard = ""
-    for i in range(0,30,5):
+    for i in range(0, 30, 5):
         scorecard += '    '+''.join(score[i:i+5])+'\n'
     return scorecard
+
 
 gameplay()
