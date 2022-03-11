8acc0dacf3f26b9d0f0a4dcd0a427c77e299ca75
Michal Fabik <mfabik@redhat.com>
2022-03-12 00:00:56 +0100

    Refactor main loop
    This commit breaks up the code into two main functions:
    - Update the letter grid and keyboard content with coloured letters etc.
      based on the player's guess.
    - Reprint the player interface with the updated data.
    The two functions' behaviour is governed by a dictionary in the top
    scope, which holds the information about the current game state.
    The main loop is moved to the top scope in the main program section and
    rearranged in a way so that each of the functions described above as
    well as the user input prompt is used exactly once in the source code.
    
    Rationale: Make game logic easier to follow and also make the source
    code more modular to make future enhancements easier to implement.

--- app.py
+++ app.py
@@ -8,105 +8,6 @@ import sys
 from boxit.boxit import boxit
 
 
-def gameplay():
-
-    if sys.version_info[0] < 3:
-        return 'You are using Python 2.x! Please switch to Python 3.6 or higher.'
-
-    word = get_word()
-    count = 0
-    grids = [["0", "0", "0", "0", "0"],
-             ["0", "0", "0", "0", "0"],
-             ["0", "0", "0", "0", "0"],
-             ["0", "0", "0", "0", "0"],
-             ["0", "0", "0", "0", "0"],
-             ["0", "0", "0", "0", "0"]]
-
-    score = ""
-
-    keyboard = "Q W E R T Y U I O P \nA S D F G H J K L ■\n■ Z X C V B N M ■ ■"
-
-    print_grid(grids)
-    print()
-    print(keyboard)
-
-    guess = input("\nTake your guess: \n ").upper()
-
-    while True:
-        if guess.lower() == 'q' or guess.lower() == 'quit':
-            print("You quit the game")
-            break
-        if guess.lower() == 'g':
-            print("You gave up! The word was: " + boxit(word, 'green'))
-            break
-        if not check_word(guess.lower()):
-            print_grid(grids)
-            print()
-            print(keyboard)
-            print("Guess not allowed! Please try again")
-            guess = input("Take your guess: \n ").upper()
-            continue
-        for position, letter in enumerate(guess):
-            if letter in word:
-                if letter == word[position]:
-                    grids[count][position] = boxit(letter, 'green')
-                    keyboard = keyboard.replace(letter, boxit(letter, 'green'))
-                    score += "g"
-                else:
-                    grids[count][position] = boxit(letter, 'yellow')
-                    if not boxit(letter, 'green') in keyboard:
-                        keyboard = keyboard.replace(letter,
-                                                    boxit(letter, 'yellow'))
-                    score += "y"
-            else:
-                grids[count][position] = letter
-                keyboard = keyboard.replace(letter, '■')
-                score += 'n'
-        print_grid(grids)
-        print()
-        print(keyboard)
-
-        if guess == word:
-            print(boxit("YOU WON!", "green", pattern='solid',
-                        shift=2, spacing=2))
-            print(boxit(f"  Total tries: {count+1}", "orange"))
-            print(display_score(score))
-            break
-        count += 1
-        if count == 6:
-            print(boxit("Sorry, you didn't make it! Better luck next time:)",
-                        'red'))
-            print("The word was: " + boxit(word, 'green'))
-            break
-        print()
-        guess = input("Take your guess: \n ").upper()
-
-
-def print_grid(grids):
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("'g' to give up or 'q' to quit\n")
-    for i in grids:
-        for j in i:
-            print(f"  {j}", end="")
-        print()
-
-
-def get_word():
-    with open('wordlist.txt', encoding='utf-8') as words:
-        wordlist = words.read().splitlines()
-        return random.choice(wordlist).upper()
-
-
-def check_word(word):
-    with open('allowed.txt', encoding='utf-8') as guess_file:
-        guesslist = guess_file.read().splitlines()
-    with open('wordlist.txt', encoding='utf-8') as words:
-        word_list = words.read().splitlines()
-    if word in guesslist or word in word_list:
-        return True
-    return False
-
-
 def display_score(score):
     colors = {'g': '🟩', 'y': '🟨', 'n': '⬜'}
     score += 'n'*(30-len(score))
@@ -119,5 +20,115 @@ def display_score(score):
     return scorecard
 
 
+def print_interface(letter_grid, keyboard, game_state, correct_answer):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("'g' to give up or 'q' to quit\n")
+
+    for i in letter_grid:
+        for j in i:
+            print(f"  {j}", end="")
+        print()
+
+    print(f"\n{keyboard}")
+
+    if game_state['invalid_word']:
+        print("Guess not allowed! Please try again")
+        game_state['invalid_word'] = False
+    else:
+        print()
+
+    if game_state['game_over']:
+        print(boxit("YOU WON!", "green", pattern='solid',
+              shift=2, spacing=2))
+        print(boxit(f"  Total tries: {game_state['attempt']}", "orange"))
+        print(display_score(game_state['score']))
+    elif game_state['attempt'] == 6:
+        print(boxit("Sorry, you didn't make it! Better luck next time:)",
+                    'red'))
+        print("The word was: " + boxit(correct_answer, 'green'))
+        game_state['game_over'] = True
+
+    return game_state
+
+
+def get_word():
+    with open('wordlist.txt', encoding='utf-8') as words:
+        wordlist = words.read().splitlines()
+        return random.choice(wordlist).upper()
+
+
+def check_word(player_guess):
+    if not player_guess:
+        return False
+    with open('allowed.txt', encoding='utf-8') as guess_file:
+        guesslist = guess_file.read().splitlines()
+    with open('wordlist.txt', encoding='utf-8') as words:
+        word_list = words.read().splitlines()
+    if player_guess in guesslist or player_guess in word_list:
+        return True
+    return False
+
+
+def update_letter_display(letter_grid, keyboard, game_state, correct_answer, player_guess):
+    for position, letter in enumerate(player_guess):
+        if letter in correct_answer:
+            if letter == correct_answer[position]:
+                letter_grid[game_state['attempt']][position] = boxit(letter, 'green')
+                keyboard = keyboard.replace(letter, boxit(letter, 'green'))
+                game_state['score'] += "g"
+            else:
+                letter_grid[game_state['attempt']][position] = boxit(letter, 'yellow')
+                if not boxit(letter, 'green') in keyboard:
+                    keyboard = keyboard.replace(letter,
+                                                boxit(letter, 'yellow'))
+                game_state['score'] += "y"
+        else:
+            letter_grid[game_state['attempt']][position] = letter
+            keyboard = keyboard.replace(letter, '■')
+            game_state['score'] += 'n'
+    return letter_grid, keyboard
+
+
 if __name__ == '__main__':
-    gameplay()
+
+    if sys.version_info[0] < 3:
+        print('You are using Python 2.x! Please switch to Python 3.6 or higher.')
+        sys.exit(1)
+
+    word = get_word()
+    grid = [["0", "0", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"]]
+
+    kbd = "Q W E R T Y U I O P \nA S D F G H J K L ■\n■ Z X C V B N M ■ ■"
+
+    state = {'attempt': 0,
+             'game_over': False,
+             'invalid_word': False,
+             'score': ''}
+
+    while True:
+        state = print_interface(grid, kbd, state, word)
+        if state['game_over']:
+            break
+        guess = input("Take your guess: \n ").upper()
+
+        if guess.lower() in ['q', 'quit']:
+            print("You quit the game")
+            break
+        if guess.lower() == 'g':
+            print("You gave up! The word was: " + boxit(word, 'green'))
+            break
+        if not check_word(guess.lower()):
+            state['invalid_word'] = True
+            # Don't count this attempt
+            continue
+
+        grid, kbd = update_letter_display(grid, kbd, state, word, guess)
+
+        if guess == word:
+            state['game_over'] = True
+        state['attempt'] += 1
